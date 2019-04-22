39f5d353e6860a075963b20f2cf71a17a7eab3c4
Stratakent <45743399+Stratakent@users.noreply.github.com>
2019-04-22 10:49:32 -0700

    Update and rename main to main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+import random
+
+#TODO: Player runs out of cards but they can still slap in, even if they slap incorrectly.
+#TODO: Player removes two card from their pile for incorrectly slapping to bottom of stack.
+#TODO: Player correctly slaps and gets all the cards in the pile.
+#TODO: Player is unable to play another face card, CPU/Player that played a face card before them gets the pile.
+#TODO: Player plays face card; CPU is unable to produce a face card the the player receives the pile.
+#TODO: Player tries to play cards when its not their turn.
+
+def main:
+    deck= ["ca", "sa", "ha", "da",
+              "c1", "s1", "h1", "d1",
+              "c2", "s2", "h2", "d2",
+              "c3", "s3", "h3", "d3",
+              "c4", "s4", "h4", "d4",
+              "c5", "s5", "h5", "d5",
+              "c6", "s6", "h6", "d6",
+              "c7", "s7", "h7", "d7",
+              "c8", "s8", "h8", "d8",
+              "c9", "s9", "h9", "d9",
+              "c10", "s10", "h10", "d10",
+              "cj", "sj", "hj", "dj",
+              "cq", "sq", "hq", "dq",
+              "ck", "sk", "hk", "dk"]
+
+    random.shuffle(deck)
+    random.shuffle(deck)
+
+    player=
+    cpu1=
+    cpu2=
+    cpu3=
+    stack=
+
+def game:
+
+
+}
